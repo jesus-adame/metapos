6ffc4d6468ada98307c3f912d2c5d2b0c724c7bf
Jesús Adame <jesus.adame.sand@gmail.com>
2024-08-09 11:52:21 -0600

    add migrations into deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 git pull origin main
 composer install -q --no-ansi --no-interaction --no-scripts --no-progress --prefer-dist
-php artisan key:generate
 chmod -R 777 storage bootstrap/cache
+php artisan key:generate
+php artisan migrate
+php artisan tenants:migrate
 npm install
 npm run build
 # Copiar los archivos build a la ubicación correcta en el servidor
