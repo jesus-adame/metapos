fac6a913e919a695686876f66981eefc4b6de645
Jesús Adame <jesus.adame.sand@gmail.com>
2024-08-09 10:02:06 -0600

    feat: update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 git pull origin main
+composer install -q --no-ansi --no-interaction --no-scripts --no-progress --prefer-dist
+php artisan key:generate
+chmod -R 777 storage bootstrap/cache
 npm install
 npm run build
 # Copiar los archivos build a la ubicación correcta en el servidor
