f84c6afa1673eca90e99efe0bda79a1f0fa84a4b
Jesús Adame <jesus.adame.sand@gmail.com>
2024-08-10 15:36:18 -0600

    fix. routes name

--- resources/js/Pages/Central/Tenants/Index.vue
+++ resources/js/Pages/Central/Tenants/Index.vue
@@ -24,7 +24,7 @@ function sendForm(tenant) {
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <Link :href="route('tenants.create')">
+                <Link :href="route('central.tenants.create')">
                     <Button label="Registrar" class="mb-4"></Button>
                 </Link>
                 <div class="bg-white overflow-hidden shadow-md sm:rounded-lg">
